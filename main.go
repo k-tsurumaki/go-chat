490df34d1cdd333fea308b65b28020ade623ced7
k-tsurumaki <kazuki67266726@gmail.com>
2025-07-12 21:13:55 +0900

    Add sample server

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+		<html>
+		<head>
+			<title>Welcome to My Web Server</title>
+		</head>
+		<body>
+			<h1>Hello, World!</h1>
+			<p>This is a simple web server written in Go.</p>
+			<p>Visit <a href="https://www.example.com">Example</a> for more information.</p>
+		</body>
+		</html>
+		`))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
